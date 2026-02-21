615492df65b464b5b510936168527686a318e55f
Aidan <duismana@gmail.com>
2026-02-20 16:20:27 -0800

    removed frontend

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,10 @@
 import asyncio
+import copy
 import json
 import uuid
-from numbers import Number
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from typing import Dict, List, Any
+from typing import Dict, Any
 
 app = FastAPI()
 
@@ -40,16 +40,120 @@ turtles: Dict[str, TurtleData] = {}
 turtles_lock = asyncio.Lock()
 
 class TurtleData:
-    def __init__(self, pos: List[Number], looking: str, agent: TurtleAgent):
-        self.pos: List[Number] = pos
-        self.looking: str = looking
+    def __init__(self, min_data: TurtleDataMin, agent: TurtleAgent):
+        self.min = min_data
         self.agent: TurtleAgent = agent
 
-world: Dict[List[Number], str] = Dict()
+    def __json__(self):
+        return self.min.__json__()
+
+world: Dict[Pos, str] = {}
 world_lock = asyncio.Lock()
 
+class TurtleDataMin:
+    def __init__(self, pos: Pos, looking: int):
+        self.pos: Pos = pos
+        self.looking: int = looking
+
+    def __json__(self):
+        return {"pos": self.pos.__json__(), "looking": self.looking}
+
+    def print(self):
+        return {"pos": self.pos.__json__(), "looking": looking_to_name(self.looking)}
+
+class Pos:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __json__(self):
+        return {"x": self.x, "y": self.y, "z": self.z}
+
 async def manage_turtle(turtle: str):
-    pass
+    await forward(turtle)
+    async with turtles_lock:
+        async with world_lock:
+            print(json.dumps((await get_min_turtle_form_turtles(turtle)).print()))
+            for i in world.values(): print(i)
+            for j in world.keys(): print(j.__json__())
+            print(len(world.values()))
+
+
+async def forward(turtle: str) -> bool:
+    suc: Dict = (await run("turtle.forward()", turtle))
+    if suc["success"]:
+        await check(turtle)
+        return True
+    else:
+        return False
+
+async def check(turtle: str):
+    for i in range(4):
+        pos: Pos = (await get_min_turtle_form_turtles(turtle)).pos
+        looking: int = (await get_min_turtle_form_turtles(turtle)).looking
+        print(looking_to_name(looking))
+        ret: dict = await run("turtle.inspect()", turtle)
+        data = ret["data"]["name"]
+        print(get_block_form_pos_and_looking(pos, looking).__json__())
+        await set_block_form_world(get_block_form_pos_and_looking(pos, looking), data)
+        await turn_left(turtle)
+
+
+async def turn_left(turtle: str):
+    await run("turtle.turnLeft()", turtle)
+    async with turtles_lock:
+        turtles[turtle].min.looking = (turtles[turtle].min.looking - 1) % 4
+
+def get_block_form_pos_and_looking(pos: Pos, looking: int) -> Pos:
+    poss: Pos = copy.deepcopy(pos)
+    looking_two: int = copy.deepcopy(looking)
+    if looking_two == 0:
+        poss.z -= 1
+    elif looking_two == 1:
+        poss.x += 1
+    elif looking_two == 2:
+        poss.z += 1
+    else:
+        poss.x -= 1
+    return poss
+
+async def run(command: str, turtle: str) -> Any:
+    async with turtles_lock:
+        it = turtles[turtle].agent.exec(command)
+    return await it
+
+async def get_min_turtle_form_turtles(turtle: str) -> TurtleDataMin:
+    async with turtles_lock:
+        return copy.deepcopy(turtles[turtle].min)
+
+async def set_turtle_pos_form_turtles(turtle_id: str, pos: Pos):
+    async with turtles_lock:
+        turtles[turtle_id].min.pos = copy.deepcopy(pos)
+
+async def set_turtle_looking_form_turtles(turtle_id: str, looking: str):
+    async with turtles_lock:
+        turtles[turtle_id].min.looking = copy.deepcopy(looking)
+
+async def get_block_form_world(block_pos: Pos) -> str:
+    async with world_lock:
+        return copy.deepcopy(world[block_pos])
+
+async def set_block_form_world(block_pos: Pos, block: str):
+    async with world_lock:
+        world[block_pos] = copy.deepcopy(block)
+
+def looking_to_name(looking: int) -> str:
+    if looking == 0:
+        return "north"
+    elif looking == 1:
+        return "east"
+    elif looking == 2:
+        return "south"
+    elif looking == 3:
+        return "west"
+    else:
+        return "AAAAAAAAAAAAAAAAAAAA"
 
 # --- API ENDPOINTS ---
 
@@ -57,7 +161,9 @@ async def manage_turtle(turtle: str):
 async def turtle_endpoint(websocket: WebSocket, turtle_id: str):
     await websocket.accept()
     agent: TurtleAgent = TurtleAgent(turtle_id, websocket)
-    turtles[turtle_id].agent = agent
+    turtle_data = TurtleData(TurtleDataMin(Pos(0,0,0), 0), agent)
+    async with turtles_lock:
+        turtles[turtle_id] = turtle_data
     print(f"Turtle '{turtle_id}' joined the swarm.")
 
     asyncio.create_task(manage_turtle(turtle_id))
@@ -68,7 +174,8 @@ async def turtle_endpoint(websocket: WebSocket, turtle_id: str):
             data = await websocket.receive_json()
             agent.resolve(data)
     except WebSocketDisconnect:
-        del turtles[turtle_id]
+        async with turtles_lock:
+            del turtles[turtle_id]
         print(f"Turtle '{turtle_id}' left.")
 
 if __name__ == "__main__":
